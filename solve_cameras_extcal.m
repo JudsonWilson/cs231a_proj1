10839420c52c719f2b5d95352df8d1f926b7471e
Judson Wilson <wilson.judson@gmail.com>
2014-03-13 23:41:00 -0700

    Solver now repositions at camera centroids.

--- solve_cameras_extcal.m
+++ solve_cameras_extcal.m
@@ -44,8 +44,52 @@ function [estimated_cameras, camera_relation_votes_and_centers] ...
 %
 
 num_cameras = correspondences.num_cameras;
+tracklets = correspondences.tracklets_cam_coords;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Tracklet Pre-Processing - Offset tracklets by camera observation
+% centroids.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Loop through each tracklet, get all tracklet x,y points, organized
+%by camera
+xy_values = cell(num_cameras,1);
+for i=1:length(tracklets)
+    t = tracklets{i};
+    cam = t.cam_num;
+    xy_values{cam} = [xy_values{cam}; t.path(:,1:2)];
+end
+%compute centroid for each camera
+for i=1:num_cameras
+    xy = xy_values{i};
+    centroids(i,1) = mean(xy(:,1));
+    centroids(i,2) = mean(xy(:,2));
+    %centroids(i,:) = [0 0]; %Hack to turn off centroids
+end
+
+% %Display all xy and the centroid
+% for i=1:1
+%     %subplot(1,2,i);
+%     hold on;
+%     xy = xy_values{i};
+%     plot(xy(:,1),xy(:,2),'.');
+%     plot(centroids(i,1),centroids(i,2),'rx');
+% end
+
+% Adjust all tracklets to remove the offset of the centroid (i.e.
+% center the data at the orign)
+for i=1:length(tracklets)
+    t = tracklets{i};
+    cam_num = t.cam_num;
+    t.path(:,1) = t.path(:,1) - centroids(cam_num,1);
+    t.path(:,2) = t.path(:,2) - centroids(cam_num,2);
+    centered_tracklets{i} = t;
+end
+clear t;
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Tracklet Processing - the actual algorithm
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -53,30 +97,30 @@ num_cameras = correspondences.num_cameras;
 %of cameras. Self-pairings should be empty. Array should be filled 
 %upper triangular - i.e. always pair the cameras lowest index to the
 % higher index, i.e. c2->c5, NOT c5->c2.
-all_camera_relation_votes = cell(num_cameras, num_cameras);
+all_centered_camera_relation_votes = cell(num_cameras, num_cameras);
 
 % Loop through all correspondences (pairings of tracklets).
 for p=1:length(correspondences.tracklet_pairings)
     pair_indices = correspondences.tracklet_pairings(p,:);
         
     %We want the lower number camera first. Swap pair if need be.
-    [~,i] = sort([correspondences.tracklets_cam_coords{pair_indices(1)}.cam_num, ...
-                  correspondences.tracklets_cam_coords{pair_indices(2)}.cam_num],'ascend');
+    [~,i] = sort([centered_tracklets{pair_indices(1)}.cam_num, ...
+                  centered_tracklets{pair_indices(2)}.cam_num],'ascend');
     pair_indices = pair_indices(i);
 
     %Do this pair now - if from different cameras:
-    c1 = correspondences.tracklets_cam_coords{pair_indices(1)}.cam_num;
-    c2 = correspondences.tracklets_cam_coords{pair_indices(2)}.cam_num;
+    c1 = centered_tracklets{pair_indices(1)}.cam_num;
+    c2 = centered_tracklets{pair_indices(2)}.cam_num;
     if c1 ~= c2
         %Feed them in to get the best camera relation from cam 1 to 2
         % Important: the first arg is tracklet from first cam
         [theta1,r,theta2] = calculate_camera_relation( ...
-                  correspondences.tracklets_cam_coords{pair_indices(1)},...
-                  correspondences.tracklets_cam_coords{pair_indices(2)});
+                  centered_tracklets{pair_indices(1)},...
+                  centered_tracklets{pair_indices(2)});
         %Only keep valid relations - if it's not valid its NaN
         if ~isnan(r)
-            all_camera_relation_votes{c1,c2} = ...
-                [all_camera_relation_votes{c1,c2}; theta1 r theta2];
+            all_centered_camera_relation_votes{c1,c2} = ...
+                [all_centered_camera_relation_votes{c1,c2}; theta1 r theta2];
         end
     end
 end    
@@ -87,25 +131,25 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 estimates_r     = inf*ones(num_cameras);
 estimates_theta = inf*ones(num_cameras);
-pairwise_camera_distance_estimates = zeros(0,3);
-pairwise_camera_angle_estimates = zeros(0,3);
+pairwise_centered_camera_distance_estimates = zeros(0,3);
+pairwise_centered_camera_angle_estimates = zeros(0,3);
 for i=1:num_cameras
     estimates_r(i,i) = 0;
     estimates_theta(i,i) = 0; %meaningless?
     for j=(i+1):num_cameras
-        e = estimate_parameters_2(all_camera_relation_votes{i,j},200,1);
+        e = estimate_parameters_2(all_centered_camera_relation_votes{i,j},200,1);
         estimates_theta(i,j) = e(1);
         estimates_r    (i,j) = e(2);
         estimates_r    (j,i) = e(2);
         estimates_theta(j,i) = e(3);
-        pairwise_camera_distance_estimates = ...
-                                  [pairwise_camera_distance_estimates; ...
+        pairwise_centered_camera_distance_estimates = ...
+                                  [pairwise_centered_camera_distance_estimates; ...
                                    i, j, estimates_r(i,j)];
-        pairwise_camera_angle_estimates = ...
-                                  [pairwise_camera_angle_estimates;
+        pairwise_centered_camera_angle_estimates = ...
+                                  [pairwise_centered_camera_angle_estimates;
                                    i, j, e(1)];
-        pairwise_camera_angle_estimates = ...
-                                  [pairwise_camera_angle_estimates;
+        pairwise_centered_camera_angle_estimates = ...
+                                  [pairwise_centered_camera_angle_estimates;
                                    j, i, e(3)];
     end
 end
@@ -123,14 +167,14 @@ end
 switch algorithm
     case 'MDS-MAP'
         estimated_locations_unreflected = cam_pos_solver_MDS_MAP(...
-                      num_cameras, pairwise_camera_distance_estimates);
+                      num_cameras, pairwise_centered_camera_distance_estimates);
     case 'SDP'
         estimated_locations_unreflected ...
             = cam_pos_solver_SDP1_alternating_anchors( ...
-                      num_cameras, pairwise_camera_distance_estimates);
+                      num_cameras, pairwise_centered_camera_distance_estimates);
     case 'LM-nllsq'
         estimated_locations_unreflected = cam_pos_solver_LM_nllsq( ...
-                      num_cameras, pairwise_camera_distance_estimates);
+                      num_cameras, pairwise_centered_camera_distance_estimates);
     otherwise
         error(['Unknown algorithm!: ' algorithm]);
 end
@@ -147,21 +191,21 @@ estimated_locations_reflected(:,1) = -estimated_locations_reflected(:,1);
 estimated_angles_unreflected = cam_angle_from_pos_solver( ...
                                       num_cameras, ...
                                       estimated_locations_unreflected, ...
-                                      pairwise_camera_angle_estimates );
+                                      pairwise_centered_camera_angle_estimates );
 estimated_angles_reflected = cam_angle_from_pos_solver( ...
                                       num_cameras, ...
                                       estimated_locations_reflected, ...
-                                      pairwise_camera_angle_estimates );
+                                      pairwise_centered_camera_angle_estimates );
 
 %
 %Compute the angle cost function for both cases
 %
 cost_unreflected = calculate_camera_angles_cost(...
-                                   pairwise_camera_angle_estimates, ...
+                                   pairwise_centered_camera_angle_estimates, ...
                                    estimated_angles_unreflected, ...
                                    estimated_locations_unreflected);
 cost_reflected = calculate_camera_angles_cost(...
-                                   pairwise_camera_angle_estimates, ...
+                                   pairwise_centered_camera_angle_estimates, ...
                                    estimated_angles_reflected, ...
                                    estimated_locations_reflected);
 
@@ -187,17 +231,20 @@ end
 fov_poly.x = [0 20 20 0];
 fov_poly.y = [0 -6  6 0];
 
-%Make an array of estimated cameras
+%Make an array of estimated cameras, making sure to undo the centering
+% step where we un-offset the centroid
 for c=1:num_cameras
     cam.fov_poly_rel = fov_poly;
-    cam.calib.x = estimated_locations(c,1);
-    cam.calib.y = estimated_locations(c,2);
+    a = estimated_angles(c);
+    rotated_centroid = [cos(a) -sin(a); sin(a) cos(a)] * centroids(c,:)';
+    cam.calib.x = estimated_locations(c,1) - rotated_centroid(1);
+    cam.calib.y = estimated_locations(c,2) - rotated_centroid(2);
     cam.calib.theta = estimated_angles(c);
     estimated_cameras(c) = camera_put_in_world(cam);
 end
 
 %Camera relation votes and centers
-camera_relation_votes_and_centers.votes = all_camera_relation_votes;
+camera_relation_votes_and_centers.votes = all_centered_camera_relation_votes;
 camera_relation_votes_and_centers.centers.theta = estimates_theta;
 camera_relation_votes_and_centers.centers.r     = estimates_r;
 
